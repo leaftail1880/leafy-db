1dfbc99b5775469db5a08f2f42fafbe3e9fd4c37
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2023-01-19 12:03:23 +0300

    2.0.1

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,18 +3,39 @@
  */
 export class DatabaseManager {
     /**
-     *
-     * @param {string} pathToRepository - In format https://github.com/leaftail1880/db/blob/ KEEP / IN END OF LINE!
+     * Creates new DatabaseManager
+     * @param {object} options
+     * @param {string} options.repositoryURL - In format https://github.com/leaftail1880/db/blob/master/
+     * KEEP "/" IN THE END OF LINE!
+     * @param {string} options.token Token with access to given repo (like "github_pat...")
+     * @param {string} options.username Keep empty for gitlab. Token's owner username
+     * @param {DatabaseManager["renderer"]} [options.renderer] Specify renderer in options instead
+     * of rewriting it by manager.renderer = ...
+     * @param {string} [options.db_filename="db.json"] Custom name for main db file
+     * @param {object} [options.commit] Adnvanced AutoCommit settings
+     * @param {number} [options.commit.minQueneSize] Minimal size for table quene to trigger commit. Default 1.
+     * @param {number} [options.commit.timerTime] Time in MS to wait until commit. Default is 1000 * 30
+     * @param {boolean} [options.reconnect] Auto-reconnect on fecth errors
      */
-    constructor(pathToRepository: string);
-    tables: {};
-    /** @type {import("./types.js").Gitrows} */
-    DB: import("./types.js").Gitrows;
+    constructor(options: {
+        repositoryURL: string;
+        token: string;
+        username: string;
+        renderer?: DatabaseManager["renderer"];
+        db_filename?: string;
+        commit?: {
+            minQueneSize?: number;
+            timerTime?: number;
+        };
+        reconnect?: boolean;
+    });
+    /** @type {Record<string, DatabaseWrapper>} */
+    tables: Record<string, DatabaseWrapper>;
     isClosed: boolean;
-    pathToRepo: string;
-    Database: DatabaseWrapper;
+    /** @type {import("./types.js").Gitrows} */
+    GitDB: import("./types.js").Gitrows;
     /**
-     *
+     * Creates a renderer to render long proccess in console. By default, renderer is disanled.
      * @param {string} postfix
      * @param {number} total
      * @example ```js
@@ -45,26 +66,40 @@ export class DatabaseManager {
      * ```
      */
     renderer(postfix: string, total: number): {
-        /**
-         *
-         * @param {number} n
-         */
-        increment(n?: number): void;
+        /** @param {number} step */
+        increment(step?: number): void;
         stop(): void;
         getProgress(): void;
         getTotal(): void;
     };
+    options: {
+        pathToRepo: string;
+        commit: {
+            queneSize: number;
+            timerTime: number;
+        };
+    };
+    Database: DatabaseWrapper<any>;
     /**
-     *
-     * @param {string} pathToFile Path to file in repo (like test.json or dir/otherdir/path.json) DONT USE ./
-     * @returns
+     * Creates a table to work with file on given path
+     * @param {string} pathToFile - Path to file in repo (like test.json or dir/otherdir/path.json) DONT USE ./
+     * @returns A table.
      */
-    CreateTable(pathToFile: string): DatabaseWrapper;
-    connect(): Promise<void>;
-    launchCommitTimer(): void;
+    CreateTable(pathToFile: string): DatabaseWrapper<any>;
+    /**
+     * Connects to the database and downloads all data of all tables to their cache
+     */
+    Connect(): Promise<void>;
+    /**
+     * Commits all tables if their quene length is more than this.minCommitQueneSize
+     */
+    commitAll(): Promise<void>;
 }
 export type StringLike = string | number | boolean;
-declare class DatabaseWrapper {
+/**
+ * @template [V=any] Save value of db. You can specify it to use type-safe db
+ */
+declare class DatabaseWrapper<V = any> {
     /**
      * @param {DatabaseManager} parent
      */
@@ -72,71 +107,96 @@ declare class DatabaseWrapper {
     /** @type {Function[]} */
     commitWaitQuene: Function[];
     _: {
-        db: DatabaseWrapper;
+        /** @private */
+        t: DatabaseWrapper<V>;
+        isConnected: boolean;
         /**
          * Trying to connect db and shows progress to console
          */
         connect(): Promise<void>;
         /**
-         * Reconects to client
+         * Reconects to db
          */
         reconnect(): Promise<void>;
         /**
-         * Closes client and quits
+         * Closes db and stop any commiting
          */
         close(): Promise<void>;
+        /**
+         * Commits all db changes
+         */
+        commit(): Promise<void>;
+        createTableFile(): Promise<any>;
+        dropTableFile(): Promise<any>;
+        openCommitTimer(): void;
+        commitTimer: any;
     };
     /**
-     *
+     * Wait until commit and then returns given value
      * @template T
      * @param {T} value
      * @returns {Promise<T>}
      */
     waitForCommit<T>(value: T): Promise<T>;
+    /**
+     * Checks if timer of dbManager is closed
+     */
     get isClosed(): boolean;
     /**
-     * Забирает данные с кэша
+     * Getting data from cache
      * @param {StringLike} key
-     * @returns {string | boolean | number | any}
+     * @returns {V}
      */
-    get(key: StringLike): string | boolean | number | any;
+    get(key: StringLike): V;
     /**
-     *
+     * Shorthand for db.get(); and db.set(); pair
      * @param {StringLike} key
-     * @returns
+     * @example ```js
+     * // Get work
+     * const {data, save} = db.work(key)
+     *
+     * // Change values
+     * data.value = 10
+     *
+     * data.obj = { value2: 1 }
+     *
+     * // Save without specify key and data
+     * save()
+     * ```
      */
-    getWork(key: StringLike): {
-        data: any;
+    work(key: StringLike): {
+        data: V;
         save: () => Promise<boolean>;
     };
     /**
-     * Запрашивает данные с датабазы
+     * Deleting data from cache and return promise that will resolve on commit
      * @param {StringLike} key
      * @returns {Promise<boolean>}
      */
     delete(key: StringLike): Promise<boolean>;
     /**
-     * Устанавливает данные в базу данных
+     * Setting data to cache and return promise that will resolve on commit
      * @param {StringLike} key
-     * @param {string | boolean | Object} value
+     * @param {V} value
      * @returns
      */
-    set(key: StringLike, value: string | boolean | any): Promise<boolean>;
+    set(key: StringLike, value: V): Promise<boolean>;
     /**
+     * Checks if cache has key
      * @param {StringLike} key
      * @returns {boolean}
      */
     has(key: StringLike): boolean;
     /**
-     * It returns an array of all the keys in the cache
+     * Returns an array of all keys in the cache
      * @returns The keys of the cache object.
      */
     keys(): string[];
     /**
-     * It returns a collection of all the keys and values in the cache
+     * Returns a collection of all keys and values in the cache
      * @returns
      */
-    collection(): Record<string, any>;
+    collection(): Record<string, V>;
     #private;
 }
 export {};
